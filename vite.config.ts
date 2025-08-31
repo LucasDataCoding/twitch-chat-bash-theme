c976536e86a5e73c337478f4d6444b4cdeb209e4
Lucas Araripe <pedro.lucx@gmail.com>
2025-08-31 11:52:59 -0300

    feature (Static Files) : Adding build folder to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,14 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // Configurações importantes para servir com Python
+  build: {
+    outDir: 'dist', // Pasta de output
+    assetsDir: 'assets', // Pasta dentro de dist para assets
+    emptyOutDir: true, // Limpa a pasta dist antes de buildar
+  },
+  // Configuração do base path
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
 })
